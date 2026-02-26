54b95cada9c387149a0c57d09788c6c3a40087f9
vth2004 <vutuanhiep9099@gmail.com>
2026-02-27 00:33:55 +0700

    add: tính năng change profile user

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -5,5 +5,5 @@ class AppConstants {
   static const int defaultTimeout = 30000;
   static const int maxImageSize = 5242880;
 
-  static const String baseUrl = 'https://63mnglpv.asse.devtunnels.ms:8080';
+  static const String baseUrl = 'https://6sd4llwt.asse.devtunnels.ms:8080';
 }
